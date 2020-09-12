a9dc47ab454a057b042c52c3f9257ec6337a9e72
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2020-09-12 21:53:23 +0300

    complete gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ const del = require("del");
 const panini = require("panini");
 const browsersync = require("browser-sync").create();
 
+/* ############# Paths ############# */
 var path = {
 	build: {
 		html: "dist/",
@@ -38,19 +39,91 @@ var path = {
 	clean: "./dist"
 }
 
+/* ############# Tasks ############# */
+function browserSync(done) {
+	browsersync.init({
+		server: {
+			baseDir: "./dist/"
+		},
+		port: 3000
+	});
+}
+
+function browserSyncReload(done) {
+	browsersync.reload();
+}
+
 function html() {
 	return src(path.src.html, { base: "src/" })
 		.pipe(plumber())
-		.pipe(dest(path.build.html));
+		.pipe(dest(path.build.html))
+		.pipe(browsersync.stream());
 }
 
 function css() {
 	return src(path.src.css, { base: "src/assets/sass/" })
 		.pipe(plumber())
 		.pipe(sass())
-		.pipe(autoprefixer())
+		.pipe(autoprefixer({
+            cascade: true
+        }))
 		.pipe(cssbeautify())
 		.pipe(dest(path.build.css))
+		.pipe(cssnano({
+			zindex: false,
+			discardComments: {
+				removeAll: true
+			}
+		}))
+		.pipe(removeComments())
+		.pipe(rename({
+			suffix: ".min",
+			extname: ".css"
+		}))
+		.pipe(dest(path.build.css))
+		.pipe(browsersync.stream());
 }
 
+function js() {
+	return src(path.src.js, { base: "./src/assets/js/" })
+		.pipe(plumber())
+		.pipe(rigger())
+		.pipe(gulp.dest(path.build.js))
+		.pipe(uglify())
+		.pipe(rename({
+			suffix: ".min",
+			extname:".js"
+		}))
+		.pipe(dest(path.build.js))
+		.pipe(browsersync.stream());
+}
+
+function images() {
+	return src(path.src.images)
+		.pipe(imagemin())
+		.pipe(dest(path.build.images));
+}
+
+function clean() {
+	return del(path.clean);
+}
+
+function watchFiles() {
+	gulp.watch([path.watch.html], html);
+	gulp.watch([path.watch.css], css);
+	gulp.watch([path.watch.js], js);
+	gulp.watch([path.watch.images], images);
+}
+
+const build = gulp.series(clean, gulp.parallel(html, css, js, images));
+const watch = gulp.parallel(build, watchFiles, browserSync);
+
+/* ############# Exports Tasks ############# */
 exports.html = html;
+exports.css = css;
+exports.js = js;
+exports.images = images;
+exports.clean = clean;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
